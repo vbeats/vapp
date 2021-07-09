ff09392e2d1ce19b5c4432ffd93298852133559e
bootvue <bootvue@gmail.com>
2021-07-09 16:00:54 +0800

    🦷  更新注释

--- src/app.config.ts
+++ src/app.config.ts
@@ -7,10 +7,10 @@ export default {
     navigationBarBackgroundColor: '#fff',
     navigationStyle: 'custom',
   },
-  // 子包不要一定是不包含在tabbar中的页面, 不要是子组件
+  // 子包pages只写子页面, 不用写子组件
   // subPackages: [
   //   {
-  //     "root": "components/",
+  //     "root": "subPackages/",
   //     "pages": [
   //       "login/index"
   //     ]
